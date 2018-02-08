3f624ad416e9f7ba803e59f81b35c25ee81fcb00
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-02-08 15:52:27 -0700

    Prettier

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 import pyglet
 
 window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, World!')
+label = pyglet.text.Label(
+            'Hello, World!',
+            font_name='Times New Roman',
+            font_size=36,
+            x=window.width//2, y=window.height//2,
+            anchor_x='center', anchor_y='center'
+        )
 
 
 @window.event
