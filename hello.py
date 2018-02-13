722e328b0f551a166e8198bfa5d8e67f7fa7ff9f
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-02-13 11:33:45 -0700

    More elaborate image blitting

--- hello.py
+++ hello.py
@@ -9,6 +9,10 @@ label = pyglet.text.Label(
             anchor_x='center', anchor_y='center'
         )
 image = pyglet.resource.image('rock.png')
+image.anchor_x = image.width // 2
+image.anchor_y = image.height // 2
+
+images = []
 
 
 def update_label(dt, text):
@@ -19,12 +23,14 @@ pyglet.clock.schedule_once(update_label, 1, text='3')
 pyglet.clock.schedule_once(update_label, 2, text='2')
 pyglet.clock.schedule_once(update_label, 3, text='1')
 pyglet.clock.schedule_once(update_label, 4, text='Go!')
+pyglet.clock.schedule_once(lambda dt: images.append((image, window.width//2, window.height * .75)), 4.5)
 
 
 @window.event
 def on_draw():
     window.clear()
-    image.blit(window.width//2, window.height * .60)
+    for image, x, y in images:
+        image.blit(x, y)
     label.draw()
 
 
