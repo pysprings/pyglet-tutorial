2f167cc5e47c7a84ea514d92f791aa529911287b
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-02-13 11:13:22 -0700

    Add timer for countdown

--- hello.py
+++ hello.py
@@ -2,13 +2,23 @@ import pyglet
 
 window = pyglet.window.Window()
 label = pyglet.text.Label(
-            'Hello, World!',
+            '',
             font_name='Times New Roman',
             font_size=36,
             x=window.width//2, y=window.height//2,
             anchor_x='center', anchor_y='center'
         )
 image = pyglet.resource.image('rock.png')
+
+
+def update_label(dt, text):
+    label.text = text
+
+
+pyglet.clock.schedule_once(update_label, 1, text='3')
+pyglet.clock.schedule_once(update_label, 2, text='2')
+pyglet.clock.schedule_once(update_label, 3, text='1')
+pyglet.clock.schedule_once(update_label, 4, text='Go!')
 
 
 @window.event
